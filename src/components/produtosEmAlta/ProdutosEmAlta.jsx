5a5b611fca9cb4a34c14e4d04dcc97a8c9e479e7
David Luan Gomes Lima <david0596@digitalcollegesul.local>
2024-06-12 15:08:55 -0300

    adcao de mais dois cards em produtos em alta e ajuste no tamanho dos cards

--- src/components/produtosEmAlta/ProdutosEmAlta.jsx
+++ src/components/produtosEmAlta/ProdutosEmAlta.jsx
@@ -92,7 +92,29 @@ export default function ProdutosEmAlta() {
                     precoOriginalSubcard="$200"
                     precoDescontoSubcard="$100"
                 />
-              </div>     
+              </div>   
+              <div className="cardsProdutosEmAlta">
+                <CardsProdutosEmAlta 
+                    promocao="30% OFF"
+                    imageSrc={TenisKSwiss}
+                    imageAlt="imagem de tenis k-swiss branco com detalhes em azul e laranja"
+                    SubDescricaoProdutosEmAlta="Tênis"
+                    descricaoProdutosEmAlta="K-Swiss V8 - Masculino"
+                    precoOriginalSubcard="$200"
+                    precoDescontoSubcard="$100"
+                />
+              </div> 
+              <div className="cardsProdutosEmAlta">
+                <CardsProdutosEmAlta 
+                    promocao="30% OFF"
+                    imageSrc={TenisKSwiss}
+                    imageAlt="imagem de tenis k-swiss branco com detalhes em azul e laranja"
+                    SubDescricaoProdutosEmAlta="Tênis"
+                    descricaoProdutosEmAlta="K-Swiss V8 - Masculino"
+                    precoOriginalSubcard="$200"
+                    precoDescontoSubcard="$100"
+                />
+              </div>   
         </>
     )
 }
